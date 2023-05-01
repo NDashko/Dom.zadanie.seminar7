9d433def0810f0c6ececf2ead826ed94f95046ae
Наталья Дашко <ndashko@mail.ru>
2023-05-01 15:14:18 +0500

    Добавлена задача на возращение значения из массива

---
+++ Task50/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void FillArray(int[,] matr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+        for (int j = 0; j < matr.GetLength(1); j++)
+            matr[i, j] = new Random().Next(0, 10);
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+            Console.Write($"{matr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк массива");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine("Введите номер строки массива ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца массива ");
+int j = Convert.ToInt32(Console.ReadLine());
+
+if (i > matrix.GetLength(0) || j > matrix.GetLength(1))
+{
+    Console.WriteLine("Такого элемента в массиве нет");
+}
+else
+{
+    Console.WriteLine($"Значение указанного вами элемента массива {matrix[i - 1, j - 1]}");
+}
